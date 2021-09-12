1143afc73d63391c812d4bd0b0d55aa40238d548
gedemais <gedemais@e1r12p19.42.fr>
2021-09-12 19:13:27 +0200

    Titles done

--- scripts/describe.py
+++ scripts/describe.py
@@ -30,6 +30,8 @@ stats = {
         "Flying": []
         }
 
+padding_offset = 7
+
 def compute_min(column):
     """ Je tiens a remercier 42 pour m'avoir permis de faire ca."""
     min_val = float('inf')
@@ -90,6 +92,44 @@ def compute_percentile(column, count, percentile):
     d1 = s[int(c)] * (k - f)
     return d0+d1
 
+################################################################################
+
+def get_column_length(stats, matiere):
+    max_length = len(matiere)
+    for stat in stats[matiere].values():
+        stat_len = len("{:.6f}".format(stat))
+        if stat_len > max_length:
+            max_length = stat_len
+    return max_length
+
+def print_features_titles(stats, lengths):
+    stdout.write(' ' * padding_offset)
+    for i, feature in enumerate(stats):
+        pad = lengths[i] - len(feature)
+        if pad > 0:
+            stdout.write(' ' * pad)
+        stdout.write(feature)
+        stdout.write(' ' * 2)
+    stdout.write('\n')
+
+
+
+def describe(stats):
+    lengths = []
+    for matiere in stats.keys():
+        lengths.append(get_column_length(stats, matiere))
+
+    print_features_titles(stats, lengths)
+    lines = ["count", "mean", "std", "min", "25%", "50%", "75%", "max"]
+
+    for line in lines:
+        stdout.write(line)
+        if len(line) < padding_offset:
+            stdout.write(' ' * (padding_offset - len(line)))
+        #for matiere in stats.keys():
+         #   print_stat_content(stats[matiere])
+        stdout.write('\n')
+
 
 def compute_stats(column):
     count = compute_count(column)
@@ -101,13 +141,21 @@ def compute_stats(column):
     fifty_pct = compute_percentile(column, count, 50)
     sf_pct = compute_percentile(column, count, 75)
 
-    return [count, mean, std, min_val, tf_pct, fifty_pct, sf_pct, max_val]
+    return {"count": count,
+            "mean": mean,
+            "std": std,
+            "min_val": min_val,
+            "tf_pct": tf_pct,
+            "fifty_pct": fifty_pct,
+            "sf_pct": sf_pct,
+            "max_val": max_val}
 
 for column in df:
     if df[column].name in stats.keys():
         stats[df[column].name] = compute_stats(df[column])
-        print(stats[df[column].name])
+        #print(stats[df[column].name])
 
+print("-------------------------------------------------------------------------")
 describe(stats)
-
+print("-------------------------------------------------------------------------")
 print(df.describe())
