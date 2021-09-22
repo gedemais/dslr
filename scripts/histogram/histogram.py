a4cd59dc1fa8725c060406143129354a39e0f8cf
jbirdqiqat <MbDBt.BN9hp@protonmail.com>
2021-09-22 23:39:40 +0200

    histogram code cleaned and commented

--- scripts/histogram/histogram.py
+++ scripts/histogram/histogram.py
@@ -1,7 +1,6 @@
-from sys import stdout, stderr, argv
+from sys import stderr, argv
 import pandas as pd
 import matplotlib.pyplot as plt
-import math
 
 def compute_count(column):
     count = 0
@@ -19,18 +18,17 @@ def compute_mean(column):
             tmp += f
     return tmp / count
 
-
 def coords(i, h):
     return int(i % h), int(i / h)
 
-
+# Histograms matrix dimensions
 hists_matrix_width = 5
 hists_matrix_height = 3
-n = hists_matrix_width * hists_matrix_height
 
-
+# Something to iterate over...
 houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
 
+# This array contains the different colors for the histogram bars for each house.
 houses_colors = {
                     "Gryffindor" : 'yellow',
                     "Hufflepuff" : 'orange',
@@ -38,6 +36,10 @@ houses_colors = {
                     "Slytherin" : 'green'
                 }
 
+# This dict contains dict-fields for each subject studied in Hogwarts.
+# Each of thoses dicts have four array-fields containing grades of each student
+# of every house in the concerned subject. This way we can access the grades of a
+# specific house in a specific subject, e.g matieres['Arithmancy']['Gryffindor']
 matieres =      {
                     "Arithmancy": {},
                     "Astronomy": {},
@@ -56,6 +58,10 @@ matieres =      {
 
 
 def get_grades(df):
+    """ 
+        This function fills the matieres dict by conditionally selecting
+        students houses and grades with df.loc() in the dataset.
+    """
     for house in houses:
         h = df.loc[df['Hogwarts House'] == house]
         for matiere in matieres.keys():
@@ -63,21 +69,23 @@ def get_grades(df):
 
 
 def plot_histograms(df):
+    """
+        This function plots a histograms matrix, containing histograms for
+        each Hogwarts subject, showing the repartition of all houses grades in
+        the concerned subject. This plot allows us to spot the subject with the
+        best grades repartition in Hogwarts.
+    """
     fig, axs = plt.subplots(hists_matrix_height, hists_matrix_width)
 
-    i = 0
-    while i < n:
-        if i >= len(matieres):
-            axs[x][y].axis("off")
-        else:
-            matiere = list(matieres)[i]
-            x, y = coords(i, hists_matrix_height)
-            axs[x][y].set_title(matiere)
-            for house in houses:
-                axs[x][y].hist( matieres[matiere][house],
-                                bins=30, alpha=0.75, color=houses_colors[house])
-        
-        i += 1
+    for i, matiere in enumerate(matieres.keys()):
+        x, y = coords(i, hists_matrix_height)
+        axs[x][y].set_title(matiere)
+        for house in houses:
+            axs[x][y].hist( matieres[matiere][house],
+                            bins=30, alpha=0.75, color=houses_colors[house])
+
+    axs[1][hists_matrix_width - 1].axis("off")
+    axs[2][hists_matrix_width - 1].axis("off")
     plt.show()
 
 
