1774ead3c56f6eec787c2a934e7f9456a441a440
jbirdqiqat <MbDBt.BN9hp@protonmail.com>
2021-09-27 20:19:39 +0200

    wrong exp value exception

--- scripts/logistic_regression/LRModel.py
+++ scripts/logistic_regression/LRModel.py
@@ -1,4 +1,5 @@
 from sys import stderr
+import pandas as pd
 import math
 
 class   LRModel():
@@ -18,9 +19,11 @@ class   LRModel():
                         "Charms",
                         "Flying"
                     ]
+    __learning_rate = 0.001
 
-    def __init__(self, n_input, weights_path=""):
+    def __init__(self, n_input, target, weights_path=""):
         try:
+            self.target = target
             self.weights = [0.0 for x in range(n_input)]
             self.output = 0.0
             self.n_input = n_input
@@ -30,6 +33,7 @@ class   LRModel():
 
 
     def __sigmoid(self, x):
+        print(x)
         self.output = 1.0 / (1.0 + math.exp(-x))
 
 
@@ -40,16 +44,44 @@ class   LRModel():
         i = 0
         sum_up = 0.0
         while i < self.n_input:
+            if pd.isna(input_data[i]):
+                i += 1
+                continue
             sum_up += input_data[i] * self.weights[i]
             i += 1
 
         self.__sigmoid(sum_up)
         return self.output
 
+    def gradient_descent(self, student, target):
+        updates = []
+        a = self.output
+        for grade in student:
+            dzw = grade
+            daz = a * (1.0 - a)
+            dca = 2.0 * (a - target)
+            pre_ret = dzw * daz
+            updates.append(pre_ret * dca)
+        return updates
+
     def train_model(self, df):
-        updates = [0.0 for x in range(self.n_input)]
-        for student in df.iterrows():
-            self.run_model(student[1].values[1:])
-            print(self.output)
-            print('-' * 80)
-                
+        error = math.inf
+        while error > 0.1:
+            updates = [0.0 for x in range(self.n_input)]
+            for student in df.iterrows():
+                house = student[1].values[0]
+                student = student[1].values[1:]
+                target = 1.0 if house == self.target else 0.0
+                self.run_model(student)
+                error = (self.output - target) ** 2.0
+                updates = self.gradient_descent(student, target)
+
+            for i, u in enumerate(updates):
+                self.weights[i] += updates[i] * self.__learning_rate
+                #
+                print("Ouput : ", self.output)
+                print("Target : ", target)
+                print("Error : ", error)
+                print('-' * 80)
+                #
+
--- scripts/logistic_regression/logreg_train.py
+++ scripts/logistic_regression/logreg_train.py
@@ -1,6 +1,14 @@
 from LRModel import LRModel
 import pandas as pd
 from sys import argv, stderr
+from time import sleep
+
+# Something to iterate over...
+models =    {
+                "Gryffindor": LRModel(13, "Gryffindor"),
+                "Hufflepuff": LRModel(13, "Hufflepuff"),
+                "Ravenclaw": LRModel(13, "Ravenclaw")
+            }
 
 def main():
     if len(argv) != 2:
@@ -10,9 +18,27 @@ def main():
     df = pd.read_csv(argv[1])
     df = df.drop(["Index", "First Name", "Last Name", "Birthday", "Best Hand"], 1)
 
-
-    model = LRModel(13)
-    model.train_model(df)
+    for model_feature in models:
+        print("Training model {0}".format(model_feature))
+        models[model_feature].train_model(df)
 
 if __name__ == "__main__":
     main()
+
+#float 	ft_compute_grad(t_env *env, int layer, int i, int j)
+#{
+#	float 	a;
+#	float 	dzw;
+#	float 	daz;
+#	float	dca;
+#	float 	pre_ret;
+#
+#	a = env->nw[layer][i].output;
+#	dzw = env->nw[layer - 1][j].output;
+#	daz = a * (1 - a);
+#	dca = 2 * (a - env->target[i]);
+#	pre_ret = dzw * daz;
+#	if (layer > 1)
+#		env->next_target[i] += (pre_ret * env->nw[layer - 1][j].weights[i]);
+#	return (pre_ret * dca);
+#}
